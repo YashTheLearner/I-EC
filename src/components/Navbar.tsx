9bfcb75e8a2bf030980d648aca68aacfd0eb1e98
Yash Rajput <yashdefying@gmail.com>
2024-12-23 14:41:11 +0530

    nav 1.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 
 function Navbar() {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
   return (
     <div className="h-12 w-full flex border-b border-gray-200 items-center justify-between px-12">
       <div>innovations@EC</div>
@@ -9,6 +11,15 @@ function Navbar() {
         <div>Blogs</div>
         <div>Team</div>
         <div>Resources</div>
+        <div>Contact us</div>
+        {isAuthenticated ?
+        <div className="flex gap-4 items-center">
+          <div className="bg-slate-200 h-6 w-6 rounded-full"></div>
+          yash
+        </div>:<div onClick={()=>{
+            setIsAuthenticated(true)
+        }}>Login</div>
+        }
       </div>
     </div>
   );
